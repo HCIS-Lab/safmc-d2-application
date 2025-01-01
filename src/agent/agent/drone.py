b25a57173194be08ce9db2269cd0789cffa65ade
LouisChang0126 <qulip.louis.0126@gmail.com>
2025-01-01 15:50:34 +0000

    change format

--- src/agent/agent/drone.py
+++ src/agent/agent/drone.py
@@ -1,6 +1,6 @@
 from rclpy.node import Node
 from transitions import Machine
-from agent_msgs.msg import Wait
+from agent_msgs.msg import CanDrop
 
 from .states import States, transitions
 
@@ -9,11 +9,15 @@ class Drone(Machine):
     def __init__(self, node: Node):
         super().__init__(self, states=States,
                          transitions=transitions, initial=States.IDLE)
-        self.wait_signal_subscription = node.create_subscription(
-            Wait, 'drop_signal_metiator', self.__set_can_drop, 10)
+        if self.get_namespace() == "drone0" or self.get_namespace() == "drone1":
+            self.wait_signal_subscription = node.create_subscription(
+                CanDrop, '/pair0/drop_signal_metiator', self.__set_can_drop, 10)
+        else:
+            self.wait_signal_subscription = node.create_subscription(
+                CanDrop, '/pair1/drop_signal_metiator', self.__set_can_drop, 10)
     
-    def __set_can_drop(self, msg: Wait):
-        self.__can_drop = (msg.wait)
+    def __set_can_drop(self, msg: CanDrop):
+        self.__can_drop = (msg.can_drop)
 
     def get_drop_signal(self):
         return self.__can_drop
