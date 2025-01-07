a90fa94f1739f308366fd1eae056288c334cd2ab
En-Fu Liao <efliao@cs.nctu.edu.tw>
2025-01-07 15:35:19 +0000

    feat: add staticmethod decorator to behavior execute

--- src/agent/agent/behavior/wait_behavior.py
+++ src/agent/agent/behavior/wait_behavior.py
@@ -3,5 +3,6 @@ from api import DroneApi, MediatorApi
 
 
 class WaitBehavior(Behavior):
+    @staticmethod
     def execute(self, drone_api: DroneApi, mediator_api: MediatorApi):
         mediator_api.wait_to_drop()
