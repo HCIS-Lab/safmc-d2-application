be32fb9428a615fc88220479571dc5e2e14a141a
En-Fu Liao <efliao@cs.nctu.edu.tw>
2025-01-10 07:53:31 +0000

    bugfix: change drop behavior execute param

--- src/agent/agent/behavior/drop_behavior.py
+++ src/agent/agent/behavior/drop_behavior.py
@@ -1,8 +1,11 @@
 from .behavior import Behavior
+
 from api import DroneApi, MediatorApi
+from rclpy.impl.rcutils_logger import RcutilsLogger
+from rclpy.clock import Clock
 
 
 class DropBehavior(Behavior):
     @staticmethod
-    def execute(self, drone_api: DroneApi, mediator_api: MediatorApi):
+    def execute(drone_api: DroneApi, mediator_api: MediatorApi, logger: RcutilsLogger, clock: Clock):
         drone_api.drop_payload()
