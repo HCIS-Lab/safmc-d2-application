b933c54c54861327bed1b9d016817c8f23783863
Luluboy168 <luluboy168@gmail.com>
2025-01-14 06:20:34 +0000

    add import

--- src/agent/agent/behavior/__init__.py
+++ src/agent/agent/behavior/__init__.py
@@ -1,4 +1,5 @@
 from .behavior import Behavior
 from .idle_behavior import IdleBehavior
+from .load_behavior import LoadBehavior
 from .wait_behavior import WaitBehavior
 from .drop_behavior import DropBehavior
