8b7dcdce91542db2c7ff6f1396a9ba64443c4606
En-Fu Liao <efliao@cs.nctu.edu.tw>
2025-01-15 12:13:18 +0000

    fix: import module error

--- src/agent/agent/behavior/__init__.py
+++ src/agent/agent/behavior/__init__.py
@@ -5,4 +5,4 @@ from .load_behavior import LoadBehavior
 from .takeoff_behavior import TakeoffBehavior
 from .wait_behavior import WaitBehavior
 from .walk_to_supply_behavior import WalkToSupplyBehavior
-from .toHotspot_behavior import ToHotspotBehavior
+from .walk_to_hotspot_behavior import WalkToHotspotBehavior
--- src/agent/agent/behavior/walk_to_hotspot_behavior.py
+++ src/agent/agent/behavior/walk_to_hotspot_behavior.py
@@ -1,25 +1,27 @@
 from .behavior import Behavior
 from agent.api.drone_api import DroneApi
-from agent.api.mediator_api import MediatorApi
-from rclpy.impl.rcutils_logger import RcutilsLogger
-from rclpy.clock import Clock
 from agent.constants import NAV_THRESH
 from agent.common.context import Context
+from typing import Optional
 
-class ToHotspotBehavior(Behavior):
+
+class WalkToHotspotBehavior(Behavior):
     @staticmethod
-    def execute(drone_api: DroneApi, mediator_api: MediatorApi, logger: RcutilsLogger, clock: Clock):
-        print("ToHotspot Behavior")
+    def execute(context: Context):
+
+        drone_api: DroneApi = context.drone_api
+
         hotspot_coord = drone_api.get_hotspot_coord()
         if drone_api.goal_arrived(hotspot_coord[0], NAV_THRESH):
             drone_api.set_hotspot_reached(True)
         else:
-            drone_api.publish_goto_setpoint(clock.now().nanoseconds, hotspot_coord[0])
+            drone_api.publish_goto_setpoint(
+                context.get_current_timestamp(), hotspot_coord[0])
             drone_api.set_hotspot_reached(False)
-    
+
     @staticmethod
-    def proceed(context: Context, agent_machine):
-        if context.drone_api.get_hotspot_reached():
-            agent_machine.drop()
-            
-    
+    def proceed(context: Context) -> Optional[str]:
+        drone_api: DroneApi = context.drone_api
+
+        if drone_api.get_hotspot_reached():
+            return "drop"
