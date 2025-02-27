3e3a48db194718cab6886071fd557c1bb84d7e8b
LouisChang0126 <qulip.louis.0126@gmail.com>
2025-02-27 16:55:28 +0000

    change node from being on a drone to being on mediator

--- src/vehicle_odometry/vehicle_odometry/vehicle_odometry.py
+++ src/vehicle_odometry/vehicle_odometry/vehicle_odometry.py
@@ -1,44 +1,49 @@
+# This ROS node should be run on mediator
+
 import rclpy
 from rclpy.node import Node
 from px4_msgs.msg import VehicleOdometry
 from uwb_msgs.msg import TagPosition
 import time
-from common.parameters import get_parameter
 
-class VisualOdometryPublisher(Node):
+class VehicleVisualOdometry(Node):
     def __init__(self):
-        super().__init__('vehicle_odometry_publisher')
-        self.eui = get_parameter(self, 'eui', '01:01')  # eui is a string from '01:01' to '04:04'
+        super().__init__('vehicle_visual_odometry')
 
         # Subscriptions
-        self.vehicle_local_position_sub = self.create_subscription(
+        self.vehicle_global_position_sub = self.create_subscription(
             TagPosition,
             "/tag_position",
             self.__set_vehicle_odometry,
             10
         )
         # Publisher
-        self.publisher_ = self.create_publisher(VehicleOdometry, f'/px4_1/fmu/in/vehicle_visual_odometry', 10)
+        self.publisher_ = [ 
+            self.create_publisher(VehicleOdometry, f'/px4_1/fmu/in/vehicle_visual_odometry', 10),
+            self.create_publisher(VehicleOdometry, f'/px4_2/fmu/in/vehicle_visual_odometry', 10),
+            self.create_publisher(VehicleOdometry, f'/px4_3/fmu/in/vehicle_visual_odometry', 10),
+            self.create_publisher(VehicleOdometry, f'/px4_4/fmu/in/vehicle_visual_odometry', 10)
+        ]
         self.timestamp = int(time.time() * 1e6)
 
     def __set_vehicle_odometry(self, msg: TagPosition):
-        if msg.eui == self.eui:
-            self.publish_odometry([msg.x, msg.y, msg.z])
+        # msg.eui is a string from '01:01' to '04:04'
+        self.publish_odometry([msg.x, msg.y, msg.z], int(msg.eui[-1]))
 
-    def publish_odometry(self, global_position):
+    def publish_odometry(self, global_position, publisher_num):
         msg = VehicleOdometry()
         msg.timestamp = self.timestamp
         msg.timestamp_sample = self.timestamp
         msg.position = global_position
         # 發布消息
-        self.publisher_.publish(msg)
-        self.get_logger().info(f'{self.eui}: Published visual odometry: {msg.timestamp}, {msg.position}')
+        self.publisher_[publisher_num - 1].publish(msg)
+        self.get_logger().info(f'px4_{publisher_num} Published visual odometry: {msg.position}')
         self.timestamp = int(time.time() * 1e6)
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = VisualOdometryPublisher()
+    node = VehicleVisualOdometry()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
