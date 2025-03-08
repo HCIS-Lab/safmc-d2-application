d1232b250a1e6d5d19e76630e70a16c6ff6dad0b
xiaomiao-fong <tomylin567@gmail.com>
2025-03-08 16:34:03 +0000

    remove unused code in odom.launch.py

--- src/vehicle_odometry/launch/odom.launch.py
+++ src/vehicle_odometry/launch/odom.launch.py
@@ -5,16 +5,13 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    pkg_dir = get_package_share_directory('vehicle_odometry')
-    params_path = os.path.join(pkg_dir, 'config', 'params.yaml')
 
     return LaunchDescription([
         Node(
             package='vehicle_odometry',
             executable='vehicle_odometry',
             name='vehicle_odometry',
-            output='screen',
-            parameters=[params_path]
+            output='screen'
         ),
     ])
 
