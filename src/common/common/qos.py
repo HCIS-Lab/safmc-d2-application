8ebae97d6231578c5550dcc9f93dc79003e8a303
Enfu Liao <enfu.liao.cs10@nycu.edu.tw>
2025-03-13 09:56:33 +0000

    fix: qos

--- src/common/common/qos.py
+++ src/common/common/qos.py
@@ -4,11 +4,6 @@ from rclpy.qos import (
     QoSProfile,
     QoSReliabilityPolicy,
 )
-<<<<<<< HEAD
-
-cmd_qos_profile = QoSProfile(
-    reliability=QoSReliabilityPolicy.BEST_EFFORT,
-=======
 
 px4_qos_profile = QoSProfile(
     reliability=QoSReliabilityPolicy.BEST_EFFORT,
@@ -26,7 +21,6 @@ cmd_qos_profile = QoSProfile(
 
 status_qos_profile = QoSProfile(
     reliability=QoSReliabilityPolicy.RELIABLE,
->>>>>>> 3e64eb3781fee90b2c315b7e3a2d97795d60924a
     durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
     history=QoSHistoryPolicy.KEEP_LAST,
     depth=1,
